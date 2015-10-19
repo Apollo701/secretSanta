03f7bb25db3dd17b7fc250bd7e482e70da2c2163
Jason Dorn <apollo701@gmail.com>
2015-10-18 18:03:18 -0700

    cleanup

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,7 +1,7 @@
 class ListsController < ApplicationController
   def new
     @list = List.new
-    4.times { @list.users.build}
+    3.times { @list.users.build }
   end
 
   def create
@@ -16,21 +16,6 @@ class ListsController < ApplicationController
       render json: @list.errors, status: :unprocessable_entity
     end
   end
-
-  # def create
-  #   @list = List.new(list_params)
-
-  #   if @list.save
-  #     # Tell the UserMailer to send a welcome email after save
-  #     @list.users.each do |user|
-  #       UserMailer.welcome_email(user).deliver_now
-  #     end
-        
-  #     render json: @list, status: :created, location: @list
-  #   else
-  #     render json: @list.errors, status: :unprocessable_entity
-  #   end
-  # end
 
   def show
     @list = List.find(params[:id])
