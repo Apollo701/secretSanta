5325c7e33c8249b3deb93e24908d6d14d62256f4
Jason Dorn <apollo701@gmail.com>
2015-10-18 17:26:24 -0700

    refactored send_emails

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,9 +9,7 @@ class ListsController < ApplicationController
 
     if @list.save
       # Tell the UserMailer to send a welcome email after save
-      for i in -1..(@list.users.size - 2)
-        UserMailer.welcome_email(@list.users[i], @list.users[i+1]).deliver_now
-      end
+      send_emails
         
       render json: @list, status: :created, location: @list
     else
@@ -43,4 +41,10 @@ class ListsController < ApplicationController
     def list_params
       params.require(:list).permit(:user, :price_limit, users_attributes: [:id, :name, :email])
     end
+
+    def send_emails
+    for i in -1..(@list.users.size - 2)
+        UserMailer.welcome_email(@list.users[i], @list.users[i+1]).deliver_now
+      end
+  end
 end
